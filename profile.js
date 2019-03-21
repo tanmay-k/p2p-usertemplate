ffbc03f8b2d1ce8f2253d6097f6f4a86e5750c38
Tanmay Kulkarni <tanmaypkulkarni@gmail.com>
2019-03-22 01:09:08 +0530

    Now you can edit your profile

--- profile.js
+++ profile.js
@@ -10,7 +10,7 @@ var renderProfile = async function()	{
 	try {
 		prof = await archive.readFile('/profile.json');//Returns string
 		profile = JSON.parse(prof);//Convert string to JSON object
-		document.querySelector('title').value = profile.name;
+		document.querySelector('title').innerHTML = profile.name;
 		document.querySelector('#avatar').src = profile.avatar;
 		document.querySelector('#user-name').value = profile.name;
 		document.querySelector('#about-user').value = profile.about;
@@ -25,7 +25,7 @@ var renderProfile = async function()	{
 renderProfile();
 ////////////////////////////////////////////////////////////////////////////////
 var enableUserEdit = function(event)	{
-	console.log(event.target.attributes);
+	//console.log(event.target.attributes);
 
 	var input = document.querySelector('#user-name');
 	input.disabled = false;//input.disabled === 'true'?false:true;
@@ -43,37 +43,82 @@ var enableAboutEdit = function()	{
 };
 
 var enableSave = function(){document.querySelector('#save-button').disabled = false;};
+var disableSave = function(){document.querySelector('#save-button').disabled = true;};
 
-var saveChanges = function()	{
+var saveChanges = async function()	{
 //	alert('GOOD!');
-	if( typeof(profile) !== 'undefined' )	{
-		profile.name = document.querySelector('#name-edit').value;
-		profile.about = document.querySelector('#about-edit').value;
+	if( typeof(updatedAvatar) !== 'undefined' )	{
+		//var path = `/${updatedAvatar.name}`;
+		var oldPath = profile.avatar;
+		var avatarImg = getNameAndExtension(updatedAvatar.name);
+		var targetPath = `/avatar.${avatarImg[1]}`;
+		var reader = new FileReader();
+		reader.onload = async function()	{
+			try {
+				//await archive.stat(path);//Check if current avatar is same as new. If not same then upload else no need to upload;
+				//var avatarImg = getNameAndExtension(updatedAvatar.name);
+				//var targetPath = `/avatar.${avatarImg[1]}`;
+				//setTimeout(async function(){await archive.writeFile(targetPath,reader.result);},200);
+				await archive.writeFile(targetPath,reader.result);
+				profile.avatar = targetPath;
+			} catch (e) {
 
-		var path = `/${updatedAvatar.files[0].name}`;
-		var oldPath = Profile.avatar;
-		if( typeof(updatedAvatar) !== 'undefined' )	{
-			var reader = new FileReader();
-			reader.onload = async function()	{
-				try {
-					await archive.stat(path);//Check if current avatar is same as new. If not same then upload else no need to upload;
-				} catch (e) {
-					setTimeout(async function(){await archive.writeFile('/',reader.result);profile.avatar = path;},500);
-				} finally {
-
+			} finally {
 				}
-			}
-			reader.readAsArrayBuffer(updatedAvatar.files[0]);
 		}
+		reader.readAsArrayBuffer(updatedAvatar);
+	}
 
-		//var newProfile = JSON.stringify(profile);
-		//await archive.unlink(oldPath);
-		//await archive.writeFile(path,newProfile);
+	profile.name = document.querySelector('#user-name').value;
+	profile.about = document.querySelector('#about-user').value;
+	var newProfile = JSON.stringify(profile);
+	await archive.unlink('/profile.json');
+	await archive.writeFile('/profile.json',newProfile);
+	await archive.configure({
+		title:'P2P-Photo Share user: '+profile.name
+	});
+	setTimeout(function(){renderProfile();},200);
+};
+
+var uploadImage = function(event)	{
+	if( event.target.files )	{
+		const {files} = event.target;
+		updatedAvatar = files[0];
+		//document.querySelector('#change-dp-modal').style.display = 'block';
+		var tempImg = document.querySelector('#temp-profile');
+
+		var reader = new FileReader()
+		reader.onload = function()	{
+			var dataURL = reader.result;
+			tempImg.src=dataURL;
+		};
+		reader.readAsDataURL(files[0]);
+		enableSave();
 	}
 };
+
+var uploadProfilePhoto = function(event)	{
+	document.querySelector('#update-dp-modal').style.display='none';
+	var avatar = document.querySelector('#avatar');
+	var img = document.querySelector('#temp-profile');
+	avatar.src = img.src;
+};
+
+var cancelImage = function()	{
+	document.querySelector('#update-dp-modal').style.display='none';
+};
+
+var getNameAndExtension = function(fileName)	{
+	extn = fileName.split('.');
+	return extn;
+}
 
 document.querySelector('#name-edit').addEventListener('click',enableUserEdit);
 document.querySelector('#about-edit').addEventListener('click',enableAboutEdit);
 document.querySelector('#name-edit').addEventListener('input',enableSave);
 document.querySelector('#about-edit').addEventListener('input',enableSave);
 document.querySelector('#save-button').addEventListener('click',saveChanges);
+
+document.querySelector('input[type="file"]').addEventListener('change', uploadImage);
+document.querySelector('#acceptBtn').addEventListener('click',uploadProfilePhoto);
+document.querySelector('#cancelBtn').addEventListener('click',cancelImage);
