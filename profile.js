334941894dde931a535ae37cbef55aff751e5469
Tanmay Kulkarni <tanmaypkulkarni@gmail.com>
2019-03-24 11:50:24 +0530

    profile photo can be updated as well

--- profile.js
+++ profile.js
@@ -47,12 +47,14 @@ var disableSave = function(){document.querySelector('#save-button').disabled = t
 
 var saveChanges = async function()	{
 //	alert('GOOD!');
+	var oldPath = profile.avatar;
+	//var avatarImg = getNameAndExtension(updatedAvatar.name);
+	var targetPath = oldPath;//`/avatar.${avatarImg[1]}`;
 	if( typeof(updatedAvatar) !== 'undefined' )	{
 		//var path = `/${updatedAvatar.name}`;
-		var oldPath = profile.avatar;
-		var avatarImg = getNameAndExtension(updatedAvatar.name);
-		var targetPath = `/avatar.${avatarImg[1]}`;
 		var reader = new FileReader();
+		avatarImg = getNameAndExtension(updatedAvatar.name);
+		targetPath = `/avatar.${avatarImg[1]}`;
 		reader.onload = async function()	{
 			try {
 				//await archive.stat(path);//Check if current avatar is same as new. If not same then upload else no need to upload;
@@ -60,9 +62,10 @@ var saveChanges = async function()	{
 				//var targetPath = `/avatar.${avatarImg[1]}`;
 				//setTimeout(async function(){await archive.writeFile(targetPath,reader.result);},200);
 				//await archive.unlink(profile.avatar);
+				await archive.unlink(profile.avatar);
 				await archive.writeFile(targetPath,reader.result);
-				profile.avatar = targetPath;
-				console.log(profile.avatar);
+				//profile.avatar = targetPath;
+				//console.log(profile.avatar);
 			} catch (e) {
 
 			} finally {
@@ -71,16 +74,25 @@ var saveChanges = async function()	{
 		reader.readAsArrayBuffer(updatedAvatar);
 	}
 
-	profile.name = document.querySelector('#user-name').value;
-	profile.about = document.querySelector('#about-user').value;
-	var newProfile = JSON.stringify(profile);
+	//profile.name = document.querySelector('#user-name').value;
+	//profile.about = document.querySelector('#about-user').value;
+	//var newProfile = JSON.stringify(profile);
+	// TODO: create new JSON object and save it instead of modifying existing object
+	var updatedProfile = {
+		'name': document.querySelector('#user-name').value,
+		'about': document.querySelector('#about-user').value,
+		'avatar': targetPath,
+		'createdAt': profile.createdAt
+	};
+	var prof = JSON.stringify(updatedProfile);
+	console.log(updatedProfile);
 	setTimeout(async function(){
-		//await archive.unlink('/profile.json');
-		await archive.writeFile('/profile.json',newProfile);
+		await archive.unlink('/profile.json');
+		await archive.writeFile('/profile.json',prof);
 		await archive.configure({
-			title:'P2P-Photo Share user: '+profile.name
+			title:'P2P-Photo Share user: '+updatedProfile.name
 		});
-		setTimeout(function(){renderProfile();},2000);},5000);
+		setTimeout(function(){profile=updatedProfile;renderProfile();},2000);},5000);
 	disableSave();
 };
 
